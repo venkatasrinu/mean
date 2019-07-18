2b363acd8aa4f938176473b2402b2482a6008f01
venkata srinivas <venkatsrinu.b@gmail.com>
2019-07-18 23:08:12 +0530

    adding mail

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var nodemailer= require('nodemailer');
+var sendgridtransport =require('nodemailer-sendgrid-transport');
 var app = router;
  var db;
 var MongoClient = require('mongodb').MongoClient;
@@ -8,11 +10,18 @@ MongoClient.connect(url,function(error,result){
   if(error){
     console.log("db error",error)
   }else{
-   // console.log("data base created")
-    db= result.db("mydb");
+    
+    db= result.db("project");
+    console.log("db created "+"connected to",db.databaseName)
     ;
   }
 })
+
+const transporter = nodemailer.createTransport(sendgridtransport({
+  auth:{
+    api_key:'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'
+  }
+}))
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -28,11 +37,24 @@ router.post("/ins",function(req,res){
     if(err){
       console.log(err,"insert err")
     }else{
-       console.log("insert result",result)
+       console.log("insertresult",result)
     }
+  })
+  transporter.sendMail({
+    to:'suryanarr@gmail.com',
+    from:'media@janasenaparty.org',
+    subject:' Mail from జనసేన సోషల్ మీడియా',
+    html:'<h1>హాయ్ Chitti గుంటూరు జిల్లా ఏరియా వింగ్ నుండి  మేము మీ facebook and WhatsApp పోస్ట్లు ప్రతి రోజు చూస్తాం చాల బాగుంటాయి, కావున మీకు ఇష్టము ఉంటే జనసేన సోషల్ మీడియాలో వార్తలు అందిస్తున్న విభాగము లో  రావాలి అని కోరిక.మరియు ఎవరికిిఐన జనసేన పార్టీని బలోపేతం చేయాలని ఊన్నా వారికి ఈ సందేశం తెలియచెయ్యండి..మీ నియోజికంలో మీరు వింగ్ అధికారిగా నియమిష్టం,<img sre><h1> <img src="https://www.janasenaparty.org/static/jspwebsitemain3/images/icon_bot.png">'
   })
   res.send("inserted successfully")
 
-})
+});
+// router.get('/getdata',function(req,res){
+//   console.log("get data working")
+//   db.collection("user").find(function(err,result){
+//     console.log(result,"result")
+//     res.send(result);
+//   })
+// })
 
 module.exports = router;
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component,OnInit} from '@angular/core';
 import { BuyService} from './buy.service';
 import { SellService} from '../services/sell'
 import {Http} from '@angular/http'
 import 'rxjs/add/operator/map';
 
-//import {map} from 'rxjs/operators'
+
 
 
 @Component({
@@ -12,51 +12,43 @@ import 'rxjs/add/operator/map';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { 
-  //title = "welcome to angular and node"
-  //sri = false;
- // today;
-// httpdata;
- // months = ["jan","feb","mar","april","may","june","july"];
- // jsonval = {name:"srinu",age:"25",mobile :{m1:"7799",m2:"5151"}}
+export class AppComponent implements OnInit { 
+ uname="";data;
   constructor (private myservice :BuyService,private sellsel :SellService,private http :Http){}
-  uname;
+  
   ngOnInit()  {
     
-    // this.myservice.sampleService();
-    //  this.today = this.sellsel.showdate()
-     console.log("in the call")
+   
+    //  console.log("in the call")
 
-     this.http.get("/getname").subscribe(data=>{
-       console.log("data",data)
-     })
+    //  this.http.get("/getname").subscribe(data=>{
+       
+    //    console.log("data",data)
+    //  })
 
     // this.http.get("http://jsonplaceholder.typicode.com/users").
     // map((Response)=> Response.json()).
     //  subscribe((data =>console.log(data)))
   }
-  // clickbutton(){
-  //   alert("hello button working")
-  //   this.sri = true;
-  // };
-// changemonth(event){
-//   alert("month changed ");
-//   alert(event.type);
-//   this.sri = false;
-//   console.log(event)
-// }
+ 
 
- //displaydata(data) {this.httpdata =data}
-  //===================//
+ 
   funins(){
     var ob={name:this.uname}
-    alert(ob.name)
-    this.http.post("/users/ins",ob).subscribe(
+
+    this.http.post("users/ins",ob).subscribe(
       dt=>{      
-      console.log(dt,"dttttttttt")
-    
-    
+      console.log(dt,"dttttttttt")    
+    this.getdata();
   })
-}
+ };
+ getdata(){
+   this.http.get("users/getdata").subscribe(
+     dt=>{
+      // this.data=JSON.parse(dt._body)
+      console.log(dt,"datadb")
+     }
+   )
+ }
 
 }
